d94e600bcbd402594ad99dda123ac266d927c356
Jozias Martini <jozias.martini@gmail.com>
2022-11-19 22:55:31 -0300

    Install and configure CORS

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+var cors = require("cors");
 const app = (0, express_1.default)();
 const port = 5000;
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
