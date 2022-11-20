1a43a64ecef7f7a39fd63d559d1795d5ceef6380
Jozias Martini <jozias.martini@gmail.com>
2022-11-20 01:53:06 -0300

    Send jwt in login route

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,10 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.post("/login", (req, res) => {
-    req.headers["content-type"] === "application/json";
-    req.headers["authorization"] === "Bearer";
-    res.send("Hello World!");
+    const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiZXhwIjoxNTE2MjM5MDIyfQ.E9bQ6QAil4HpH825QC5PtjNGEDQTtMpcj0SO2W8vmag";
+    res.send({
+        token: jwt,
+    });
 });
 app.listen(port, () => {
     console.log(`Auth app listening on port ${port}`);
