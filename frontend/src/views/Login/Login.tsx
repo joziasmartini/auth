31f19d2a49644230fbd861a871e5264ee77cd994
Jozias Martini <jozias.martini@gmail.com>
2022-11-19 23:54:02 -0300

    Update colors and spacing

--- frontend/src/views/Login/Login.tsx
+++ frontend/src/views/Login/Login.tsx
@@ -38,11 +38,11 @@ export default function Login() {
           <div className="xl:w-10/12">
             <div className="block bg-white shadow-lg rounded-lg">
               <div className="lg:flex lg:flex-wrap g-0">
-                <div className="lg:w-6/12 px-4 md:px-0">
+                <div className="lg:w-6/12 p-8 md:px-0">
                   <div className="md:p-12 md:mx-6">
                     <div className="text-center">
                       <div className="grid place-content-center my-6">
-                        <LockKey size={58} />
+                        <LockKey size={58} color="#0f766e" />
                       </div>
                       {/* <h4 className="text-4xl font-bold mt-6 mb-6 pb-1">
                         Auth
@@ -55,7 +55,7 @@ export default function Login() {
                       <div className="mb-4">
                         <input
                           type="text"
-                          className="form-control block w-full px-6 py-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          className="form-control block w-full px-6 py-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-sky-700 focus:outline-none"
                           id="exampleFormControlInput1"
                           placeholder="Username"
                           onChange={(e) => setUsername(e.target.value)}
@@ -64,7 +64,7 @@ export default function Login() {
                       <div className="mb-4">
                         <input
                           type="password"
-                          className="form-control block w-full px-6 py-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                          className="form-control block w-full px-6 py-3 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-sky-700 focus:outline-none"
                           id="exampleFormControlInput2"
                           placeholder="Password"
                           onChange={(e) => setPassword(e.target.value)}
@@ -72,7 +72,7 @@ export default function Login() {
                       </div>
                       <div className="text-center pt-1 mb-6 pb-1">
                         <button
-                          className="inline-block px-6 py-4 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+                          className="inline-block px-6 py-4 text-white font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 bg-gradient-to-r from-sky-700 to-emerald-700"
                           type="button"
                           data-mdb-ripple="true"
                           data-mdb-ripple-color="light"
@@ -81,7 +81,11 @@ export default function Login() {
                           Log in
                         </button>
                         <a className="text-gray-500" href="#!">
-                          Forgot password?
+                          recover account
+                        </a>
+                        <span> or </span>
+                        <a className="text-gray-500" href="#!">
+                          create account
                         </a>
                       </div>
                       <div className="flex items-center justify-between pb-6">
@@ -98,8 +102,8 @@ export default function Login() {
                     </form>
                   </div>
                 </div>
-                <div className="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-                  <div className="text-white px-4 py-6 md:p-12 md:mx-6">
+                <div className="lg:w-6/12 p-4 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none bg-gradient-to-r from-sky-700 to-emerald-700">
+                  <div className="text-white px-4 py-6 md:p-12 md:mx-0">
                     <h4 className="text-2xl font-semibold mb-6">
                       We are more than just a company
                     </h4>
